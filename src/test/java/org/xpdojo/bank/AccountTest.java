8a8927d89ff79b2d39eb6755892f729bbcaa9209
KernerHR <KernerHR@gmx.de>
2020-08-06 08:21:34 +0200

    MyFirstTestCommit

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+// my first change
 public class AccountTest {
 
     @Test
